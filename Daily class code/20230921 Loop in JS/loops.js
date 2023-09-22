a9d1713a2f34b171e996f8e7d7ae1475020d7438
vinay <vinay.shermauryan@gmail.com>
2023-09-22 08:28:51 +0530

    more in loop

--- Daily class code/20230921 Loop in JS/loops.js
+++ Daily class code/20230921 Loop in JS/loops.js
@@ -63,3 +63,96 @@ number ++
 2         20          20 > 2          2, 20
 2         19           19 > 1         2, 19
 */
+
+// write a program to print number from 1 to 100
+for (let num = 1; num <= 100; num++) {
+  console.log("number from 1 to 100", num);
+}
+
+// write a program to print table of 2
+for (let i = 1; i <= 10; i++) {
+  console.log("i", i);
+  console.log(`tabel of 2X${i} =>`, 2 * i);
+}
+// 2X1 = 2
+// 2X2 = 4
+// 2X3 = 6
+// 2X4 = 8
+// 2X5 = 10
+// 2X6 = 12
+// 2X7 = 14
+// 2X8 = 16
+// 2X9 = 18
+// 2X10 = 20
+// write a program to print table of 3
+// for (let i = 1; i <= 10; i++) {
+//   console.log("i", i);
+//   console.log(`tabel of 3X${i} =>`, 3 * i);
+// }
+
+// table from 1 to 10
+// logic ----
+/*
+1X1 = 1     2
+1X2 = 2
+1X3 = 3
+1X4 = 4
+1X5 = 5
+1X6 = 6
+......
+....
+1X10 = 10
+// 2X1 = 2
+// 2X2 = 4
+// 2X3 = 6
+// 2X4 = 8
+// 2X5 = 10
+// 2X6 = 12
+// 2X7 = 14
+// 2X8 = 16
+// 2X9 = 18
+// 2X10 = 20
+
+// 3X1 = 3
+// 3X2 = 6
+// 3X3 = 9
+// 3X4 = 12
+// 3X5 = 15
+// 3X6 = 18
+// 3X7 = 21
+// 3X8 = 24
+// 3X9 = 27
+// 3X10 = 30
+....
+*/
+
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`table ${i} X ${j}`, i * j);
+  }
+}
+/* 1st loop of i call --
+i  condition   j   condition       result
+1   1 <= 10    1    1<=10           1X1 = 1
+1   ......     2    2 <= 10         1X2 = 2
+1   ......     3    3 <= 10         1X3 = 3
+..............................
+.........................
+1   .........  9     9 <= 10        1X9 = 9
+1   .........  10    10 <= 10        1X10 = 10
+1  .........   11    11<=10 
+
+2nd loop of i ---
+2   2<=10     1     1 <= 10        2X1 = 2
+2   ....      2     2 <=10         2X2 = 4
+.................
+..................................
+*/
+
+
+// 1. write a program to print table from 20 to 30 using nested for loop
+// 2. Write a program to find the average of three number , a=50,b=60,c=70
+// 3. Write a program to find even number and odd number between 1 to 50
+// 4. Write a program to find the largest number between two numbers number1 = 80, number2 = 90
+// 5. Write a program to print that student is present in class or absent by using switch case statement
+// ex -> Ramesh = Absent, Suresh = Present, Mahesh = Present
