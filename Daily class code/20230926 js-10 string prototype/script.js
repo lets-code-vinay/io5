d36c928b00fc1f089e42a88d5dd03bd90dfbe8a4
vinay <vinay.shermauryan@gmail.com>
2023-10-05 21:32:19 +0530

    string prototypes-2

--- Daily class code/20230926 js-10 string prototype/script.js
+++ Daily class code/20230926 js-10 string prototype/script.js
@@ -31,13 +31,30 @@
  * str + " " +str2 :: to concat two or more strings
  * str.includes()  :: check if string is available or not
  * str.search()  :: check if string is available or not and return the index value
- *
+ * str.split()  :: to make array from string
+ * str.reverse()  :: to reverse array of string (array prototype)
+ * str.join()  :: to join array of string (array prototype)
+ * str.repeat(2) :: is used to repeat any string as per given number
+ * str.indexOf('') :: is used to get the index value of any string character
+ * str.lastIndexOf('') :: is used to get the index value from the end of any string character
+ * str.startsWith('') :: is used to check the characters are in starting or not
+ * str.endsWith('') :: is used to check the characters are in ending or not
+ * str.trim() :: is used to remove extra spaces from start and end of string.
+ * str.trimStart() :: is used to remove extra spaces from start of string.
+ * str.trimEnd() :: is used to remove extra spaces from  end of string.
+ * str.padStart(length++ , '*') :: add extra characters at starting when string length got exceed
+ * str.padEnd(length++ , '*') :: add extra characters at end when string length got exceed
+ * str.slice(2) :: use to cut/slice string and return the string after the given index
+ * str.slice(2, 6) :: use to cut/slice string and return the string in between the given index
+ * str.subString(2) :: use to cut/slice string and return the string after the given index
+ * str.subString(2, 6) :: use to cut/slice string and return the string in between the given index
+ 
  */
 const str = `Very Short Stories 
 with Morals in English are the best ways to teach moral education for kids and adults. You can teach short stories with moral values in a very short time.
 In this story page, very short stories in English with moral are given with pictures and PDFs to download in English.          `;
 
-console.log("1--- lenght", str.length);
+console.log("1--- length", str.length);
 
 let str1 = "I have an Iphone";
 
@@ -146,3 +163,71 @@ console.log("25 --->includes--", str10.includes("a"));
 console.log("26 --->includes--", str10.includes("morala"));
 
 console.log("27 --->search--", str10.search("ery"));
+
+console.log("28 ---> split <---", str10.split(""));
+
+const splittedStr = str10.split(" ");
+// console.log("29 ---> splittedStr <---", splittedStr);
+
+const revString = splittedStr.reverse();
+console.log("29.1 ---> reverse <---", revString);
+console.log("29.2 ---> join <---", revString.join(" "));
+
+// console.log("30 ---> split <---", str10.split("a"));
+
+// 1. split
+// 2. reverse
+// 3. join
+
+let name;
+name = "Vinay Maurya"; // ayruaM yaniV  // Maurya Vinay
+let num1 = 567891; // 198765
+
+name = String(name).split("");
+name = name.reverse();
+name = name.join("");
+
+console.log(" 30-=-->", name);
+
+let str11 = " Remote";
+console.log("31----> repeat() <--", str11.repeat(15));
+console.log(`i said give me${str11.repeat(3)}`);
+
+const str12 =
+  "pictures with given are moral with English in stories with short very";
+console.log("32 ---> indexOf--", str12.indexOf("with"));
+console.log("33 ---> lastIndexOf--", str12.lastIndexOf("with"));
+
+const str13 = "My name is Johny!";
+console.log("34---> startsWith < ==", str13.startsWith("My name"));
+console.log("35---> endsWith < ==", str13.endsWith("sunny!"));
+console.log("36---> endsWith < ==", str13.endsWith("Johny!"));
+console.log("37---> endsWith < ==", str13.endsWith("!"));
+
+const str14 = "   My name is Johny!      ";
+console.log(`38---->`, str14.trim());
+
+const str15 = "         My name is Sunny !            ";
+console.log(`39---->`, str15.trimStart());
+
+const str16 = "         My name is Sunny !            ";
+console.log(`39---->`, str16.trimEnd());
+
+const str17 = "The burning desires!!"; // XXXXThe burning desires!! // 21
+const strPadding = str17.padStart(str17.length + 1, "*");
+console.log(`40---->`, strPadding);
+
+const str18 = "The burning desires!!"; // The burning desires!! // 21
+const strPaddingEnd = str18.padEnd(str18.length + 5, "*");
+console.log(`41---->`, strPaddingEnd);
+
+const str19 = "The burning desires!!";
+const slicedStr = str19.slice(6, 12);
+console.log("42----> sliced---", slicedStr);
+
+const str20 = "The burning desires!!";
+let subStr20 = str20.substring(6);
+console.log("43----> substring---", str20.at(15), subStr20);
+
+subStr20 = str20.substring(6, 15);
+console.log("44----> substring---", subStr20);
